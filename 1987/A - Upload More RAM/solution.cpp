55503bd7aeab749477f9c413cd832611ea4418f7
Akshat Jain <fiercfly@gmail.com>
2026-09-13 15:48:54 +0530

    Add Upload More RAM [A] from Codeforces

---
+++ 1987/A - Upload More RAM/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int result=n+(n-1)*(k-1);
+        cout <<result<<endl;
+    }
+    return 0;
+}
