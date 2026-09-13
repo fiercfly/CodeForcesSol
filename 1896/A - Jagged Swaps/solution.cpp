e40c942e4c89b107e4b9eb84e67a7e6fb8d79cdd
Akshat Jain <fiercfly@gmail.com>
2026-09-13 16:00:11 +0530

    Add Jagged Swaps [A] from Codeforces

---
+++ 1896/A - Jagged Swaps/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+ 
+        bool sorted= true;
+ 
+        while(true){
+            bool found= false;
+            for(int i=1; i<n-1; i++){
+                if(arr[i-1] < arr[i] && arr[i] > arr[i+1]){
+                    found= true;
+                    int temp= arr[i];
+                    arr[i]= arr[i+1];
+                    arr[i+1]= temp;
+                }
+            }
+            if(!found){
+                //check if its sorted or not
+                for(int i=1; i<n; i++){
+                    if(arr[i] <= arr[i-1]){
+                        sorted= false;
+                    }
+                }
+                break;
+            }
+        }
+        if(sorted){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        // cout<<sorted==true?"YES":"NO"<<endl;
+    }
+ 
+    return 0;
+}
